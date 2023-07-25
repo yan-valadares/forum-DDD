46b5730c9934ca41e3f17901a9c2408d598a416b
Yan Valadares <yancardoso@outlook.com.br>
2023-07-25 14:31:04 -0300

    Refatoração de estrutura de pastas

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -1,5 +1,5 @@
-import { UniqueEntityId } from '../../core/entities/unique-entity-id'
-import { Answer } from '../entities/answer'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Answer } from '../../enterprise/entities/answer'
 import { AnswerRepository } from '../repositories/answers-repository'
 
 interface AnswerQuestionUseCaseRequest {
